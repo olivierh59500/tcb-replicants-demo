be91599f7d452055aeffc7b391f094df70962703
Olivier Houte <github@houte.com>
2026-09-23 14:37:21 +0200

    Share font tile caching and alphabet lookup

---
+++ dck/font_compatibility_test.go
@@ -0,0 +1,67 @@
+package tcbreplicants
+
+import "testing"
+
+func legacyAtlasIndex(ch rune) (int, bool) {
+	switch ch {
+	case '!':
+		return 1, true
+	case '"':
+		return 2, true
+	case '\'':
+		return 7, true
+	case '(':
+		return 8, true
+	case ')':
+		return 9, true
+	case ',':
+		return 12, true
+	case '-':
+		return 13, true
+	case '.':
+		return 14, true
+	case '0':
+		return 16, true
+	case '1':
+		return 17, true
+	case '2':
+		return 18, true
+	case '3':
+		return 19, true
+	case '4':
+		return 20, true
+	case '5':
+		return 21, true
+	case '6':
+		return 22, true
+	case '7':
+		return 23, true
+	case '8':
+		return 24, true
+	case '9':
+		return 25, true
+	case ':':
+		return 27, true
+	case ';':
+		return 28, true
+	case '?':
+		return 31, true
+	case 'A', 'B', 'C', 'D', 'E', 'F', 'G':
+		return 33 + int(ch-'A'), true
+	case 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q':
+		return 40 + int(ch-'H'), true
+	case 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z':
+		return 50 + int(ch-'R'), true
+	default:
+		return 0, false
+	}
+}
+func TestSharedAtlasIndicesMatchOriginalAlphabet(t *testing.T) {
+	for r := rune(0); r < 256; r++ {
+		got, ok := charToFontIndex(r)
+		want, found := legacyAtlasIndex(r)
+		if got != want || ok != found {
+			t.Fatalf("rune %U: got (%d,%t), want (%d,%t)", r, got, ok, want, found)
+		}
+	}
+}
--- dck/game.go
+++ dck/game.go
@@ -4,6 +4,7 @@ package tcbreplicants
 import (
 	"bytes"
 	"fmt"
+	"github.com/olivierh59500/democonstructionkit/presets"
 	originalassets "tcb-replicants-demo"
 
 	"github.com/olivierh59500/democonstructionkit/sound"
@@ -237,12 +238,9 @@ func (g *Game) initScrollText() {
 		charsPerRow = 10
 	)
 	glyphCount := (g.scrollFont.Bounds().Dy() / charHeight) * charsPerRow
-	glyphs := make([]*ebiten.Image, glyphCount)
-	for index := range glyphs {
-		row := index / charsPerRow
-		column := index % charsPerRow
-		rect := image.Rect(column*charWidth, row*charHeight, (column+1)*charWidth, (row+1)*charHeight)
-		glyphs[index] = g.scrollFont.SubImage(rect).(*ebiten.Image)
+	glyphs, err := scrolling.GridImages(g.scrollFont, image.Pt(charWidth, charHeight), charsPerRow, glyphCount)
+	if err != nil {
+		panic(err)
 	}
 
 	tiles := textTiles(text)
@@ -398,60 +396,13 @@ func (g *Game) drawStarfield(dst *ebiten.Image) {
 	}
 }
 
-func charToFontIndex(ch rune) (int, bool) {
-	switch ch {
-	case '!':
-		return 1, true
-	case '"':
-		return 2, true
-	case '\'':
-		return 7, true
-	case '(':
-		return 8, true
-	case ')':
-		return 9, true
-	case ',':
-		return 12, true
-	case '-':
-		return 13, true
-	case '.':
-		return 14, true
-	case '0':
-		return 16, true
-	case '1':
-		return 17, true
-	case '2':
-		return 18, true
-	case '3':
-		return 19, true
-	case '4':
-		return 20, true
-	case '5':
-		return 21, true
-	case '6':
-		return 22, true
-	case '7':
-		return 23, true
-	case '8':
-		return 24, true
-	case '9':
-		return 25, true
-	case ':':
-		return 27, true
-	case ';':
-		return 28, true
-	case '?':
-		return 31, true
-	case 'A', 'B', 'C', 'D', 'E', 'F', 'G':
-		return 33 + int(ch-'A'), true
-	case 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q':
-		return 40 + int(ch-'H'), true
-	case 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z':
-		return 50 + int(ch-'R'), true
-	default:
-		return 0, false
+var charToFontIndex = func() func(rune) (int, bool) {
+	lookup, err := presets.TileLookup("tcb-replicants-demo", true)
+	if err != nil {
+		panic(err)
 	}
-}
+	return lookup
+}()
 
 func textTiles(text string) []int {
 	tiles := make([]int, 0, len(text))
